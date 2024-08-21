177f7a41e928cd8abaafb80eccff279fa000296f
TheWitchs23 <diarhe03@gmail.com>
2024-08-21 12:29:57 -0600

    Delete Sistema Wwb directory

--- Sistema Wwb/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli=new mysqli("localhost", "root", "", "dbintegrative");
-if($mysqli->connect_errno)
-{
-echo "No se pudo conectar con la base de datos:(".$mysqli->connect_errno.")".$mysqli->connect_error;
-}
-?>
